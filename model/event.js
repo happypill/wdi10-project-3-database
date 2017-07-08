d35bca81fe8a437bbe885f42294a5bcd633870ec
rubyflames <rubyflames@users.noreply.github.com>
2017-07-08 14:42:24 +0800

    passport neatened, eventPassed added to event model

--- model/event.js
+++ model/event.js
@@ -4,10 +4,13 @@ const user = require('./user');
 const eventSchema = new mongoose.Schema({
   name: String,
   venue: Number,
+  date: String,
+  time: String,
   joinedIndividual: [{ type: Schema.Types.ObjectId, ref: 'user' }],
   organiser: [{ type: Schema.Types.ObjectId, ref: 'user' }]
-  imageURL:String,
-  details:String
+  imageURL: String,
+  details: String,
+  eventPassed: Boolean
 },{timestamp: true})
 
 
