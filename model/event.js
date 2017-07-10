7aef91eb1267bf426e5a9556018565767befc5eb
rubyflames <rubyflames@users.noreply.github.com>
2017-07-10 10:31:45 +0800

    uuid added to event model

--- model/event.js
+++ model/event.js
@@ -1,12 +1,14 @@
+import uuid from 'uuidv4';
 import mongoose from 'mongoose';
 const user = require('./user');
 
 const eventSchema = new mongoose.Schema({
+  eventID: { type: Schema.Types.ObjectId, ref: 'uuid' },
   name: String,
   venue: Number,
   date: String,
   time: String,
-  joinedIndividual: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  attending: [{ type: Schema.Types.ObjectId, ref: 'user' }],
   organiser: [{ type: Schema.Types.ObjectId, ref: 'user' }]
   imageURL: String,
   details: String,
