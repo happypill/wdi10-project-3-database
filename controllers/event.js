5f122a8990eb89c6325c64590d95c7d7f06d236a
kyozone <judolew@gmail.com>
2017-07-13 03:11:34 +0800

    Update Event ROutes

--- controllers/event.js
+++ controllers/event.js
@@ -1,15 +1,159 @@
-const Event = require('../models/event')
+import Event from '../model/Event';
+import User from '../model/User';
 
-// GET all
-exports.getAll = (req, res) => {
-  Event.find((err, events) => {
-    if (err) res.json({message: 'could not find event because: ' + err})
-    console.log('getting all')
-    res.render('home', {
-      events: events
-    })
-  })
+
+/*
+* Get books
+*/
+exports.getAllEvents = (req, res, next) => {
+
+  console.log("Request All Events");
+
+  Event.find()
+    .populate('attending')
+    .populate('organiser')
+    .exec((err, event) => {
+      if (err) return res.status(404).send('Not found');
+      res.json(event);
+    });
 }
 
-//from bookmarkslist.js under controllers
-// const User = require('../models/user');
+/*
+*  Get a book
+*/
+exports.getEvent = (req, res, next) => {
+
+  console.log("Request one Event");
+
+  const id = req.params.id;
+  Event.findById(id)
+    .populate('attending')
+    .populate('organiser')
+    .exec((err, event) => {
+      if (err) return res.status(404).send('Not found');
+      res.json(event);
+    });
+}
+
+
+/*
+*  Create Event, update User with Organiser
+*/
+exports.createEvent= (req, res, next) => {
+
+  console.log("Create Event : Add Organiser to User");
+
+ 
+    const event = new Event();
+    event.name = req.body.name || "Unknown";
+    event.venue = req.body.venue || "Unknown";
+    event.start = req.body.start || "-1";
+    event.end = req.body.end || "Unknown";
+    event.details = req.body.details || "-1";
+      event.save((err, event) => {
+
+        /* Update user model with Organiser  */
+        const user = req.user;
+        console.log("Add Organiser to User Model);
+        console.log(user);
+
+        User.findById(user._id, (err, foundUser) => {
+          if (err) return res.status(400).send('Bad Request');
+
+          if(!foundUser){
+            return res.status(404).send('User not Found');
+          }
+
+          foundUser.organiser.push(user._id);
+          foundUser.save( (err, savedUser) => {
+            if (err) return res.status(400).send('Bad Request');
+            console.log("Push organiser._id into User organiser");
+            console.log(savedUser);
+          });
+
+        });
+           res.json(book);
+      });
+}
+
+/*
+* Update Event
+*/
+exports.updateEvent = (req, res, next) => {
+
+  console.log("Request : UpdateEvent");
+
+
+
+    const id = req.params.id;
+    const event = req.body;
+
+   Event.findById(id, (err, foundEvent) => {
+       if (err) return res.status(400).send('Bad Request');
+
+       if(!foundEvent){
+         return res.status(404).send('Not Found');
+       }
+       console.log("Found Event,Proceed to Update")
+
+       foundEvent.name = event.name;
+       foundEvent.venue = event.venue;
+       foundEvent.start = event.start;
+       foundEvent.end = event.end;
+       foundEvent.details = event.details;
+       if(event.release == 'true') {
+
+         const organisedId = foundEvent.organiser
+
+         User.findById(organisedId, (err, foundEvent) => {
+           if (err) return res.status(400).send('Bad Request');
+
+           if(!foundEvent){
+             return res.status(404).send('Organiser not found.It may be deleted');
+           }
+           console.log("Found Organiser");
+
+           const userAttending = foundEvent.attending;
+           userAttending.splice(userAttending.indexOf(organisedId),1);
+
+           foundBorrower.save( (err, saveAttending) => {
+             if (err) return res.status(400).send('Bad Request');
+             console.log("Remove field Attending from User");
+             console.log(saveAttending);
+           });
+         });
+
+         foundEvent.reservedBy = null;
+         foundEvent.reserved = false;
+       }
+       console.log(foundEvent);
+       foundEvent.save((err, updatedEvent)=> {
+         if (err) return res.status(400).send('Bad Request');
+         console.log("Updated Event");
+         res.json(updatedEvent);
+       });
+    });
+
+}
+
+/*
+*  Delete Event
+*/
+exports.deleteEvent = (req, res, next) => {
+
+  const id = req.params.id;
+  Event.findById(id, (err, event) => {
+    if (err) return res.status(400).send('Bad Request');
+
+    if(!event){
+      return res.status(404).send('Not Found');
+    }
+
+    event.remove();
+
+    res.json({'message':"event deleted"});
+  });
+}
+
+
+export default exports;
--- routes/event.js
+++ routes/event.js
@@ -1,82 +1,21 @@
-
 import express from 'express';
-import Event from '../model/event';
+import eventController from '../controller/event';
+
+/* middleware for file handling en route to cloudinary */
+import multer from 'multer';
+const upload = multer({ dest: './uploads/' });
 
 const router = express.Router();
 
-/*Send Transaction information from database to frontend*/
-router.get('/', (req, res, next) => {
-  Event.find({}, (err, event) => {
-    res.json(event);
-  });
+router.get('/event', eventController.getAllEvents);
+router.get('/event/:id', eventController.getEvent);
+router.post('/event', eventController.createEvent);
+router.put('/event/:id',eventController.updateEvent);
+router.delete('/event/delete', eventController.deleteEvent);
 
-});
-
-router.post('/', (req, res, next) => {
-    const event = new Event();
-    event.name = req.name
-      // venue:'Orchard Road',
-      // joinedIndividual:'John',
-      // imageUrl:'fdfdfdfd',
-      // details:'Bring an apple'
-    event.save();
-
-})
-
-/*
- *  Create
- */
- router.post('/', (req, res, next) => {
-
-    const event = new Event();
-    event.name = req.body.name || "Unknown";
-    event.venue = req.body.venue || "Unknown";
-    event.joinedIndividual = req.body.joinedIndividual || "-1";
-    event.imageURL = req.body.imageURL || "Unknown";
-    event.details = req.body.details || "-1";
-    event.save((err, event) => {
-         res.json(event);
-    });
- });
-/*
-  *  Read
-  */
-  router.get('/:id', (req, res, next) => {
-
-    const id = req.params.id;
-    Event.findById(id, (err, event) => {
-      if (err) return res.status(404).send('Not found');
-      res.json(event);
-    });
-  });
-/*
-   *  Update
-   */
-   router.put('/:id', (req, res, next) => {
-     console.log("Got PUT Request");
-
-     const event = req.body.event;
-
-     Car.findById(event._id, (err, foundEvent) => {
-        if (err) return res.status(400).send('Bad Request');
-
-        if(!foundCar){
-          return res.status(404).send('Not Found');
-        }
-
-        foundEvent.manufacturer = event.name;
-        foundEvent.model = event.venue;
-        foundEvent.year = event.joinedIndividual;
-        foundEvent.color = event.imageURL;
-        foundEvent.kms = event.details;
-    
-        foundEvent.save((err, car)=> {
-          if (err) return res.status(400).send('Bad Request');
-          res.json(foundEvent);
-        });
-
-     });
-   });
 
 
 export default router;
+
+
+
