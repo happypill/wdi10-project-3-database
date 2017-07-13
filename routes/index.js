0e9d92c32869d9b362b183b957ee7a312a1c5b72
N=2 <judolew@gmail.com>
2017-07-13 14:23:54 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,6 @@ import User from '../model/user';
 
 const router = express.Router();
 
-// /*Send Transaction information from database to frontend*/
-// router.get('/apiTransaction', (req, res, next) => {
-//   Transaction.find({}, (err, users) => {
-//     res.json(users);
-//   });
-//
-// });
-//
-// /*Send Inventory information from database to frontend*/
-// router.get('/apiInventory', (req, res, next) => {
-//   Inventory.find({}, (err, users) => {
-//     res.json(users);
-//   });
-//
-// });
-
 router.get('/', (req, res, next) => {
   res.render('index', {
     title: 'Express'
